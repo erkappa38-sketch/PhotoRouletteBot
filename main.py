dd1e6e0a30d63ccd4da3fc81e709db7e0158d292
erkappa38-sketch <erkappa38@gmail.com>
2026-07-28 20:43:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from bot import run_bot
-import asyncio
+from bot import app_web
 
 if __name__ == "__main__":
-    asyncio.run(run_bot())
+    import os
+
+    port = int(os.environ.get("PORT", 10000))
+
+    app_web.run(
+        host="0.0.0.0",
+        port=port
+    )
